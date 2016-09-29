e655ed86fb51bc6c68ddf3e120175427a8c70279
Niels de Rijk <niels@blocklevel.nl>
2016-09-29 11:10:47 +0200

    Added result screen

---
+++ frontend/src/app/pages/quiz/result/result.js
@@ -0,0 +1,8 @@
+// import loader from './../../../utils/loader';
+// import layout from '../../../layouts/quiz/quiz';
+import { TweenMax, TimelineMax } from 'gsap';
+import _ from 'lodash';
+
+export default {
+
+};
---
+++ frontend/src/app/pages/quiz/result/result.vue
@@ -0,0 +1,3 @@
+<style src="./result.scss" lang="scss"></style>
+<template src="./result.html"></template>
+<script src="./result.js" lang="babel"></script>
